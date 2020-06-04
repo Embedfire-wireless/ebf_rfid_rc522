a23b73a8ca4d3a07506ff0ea95abff6e99556a56
Yuhailong <2816850683@qq.com>
2020-06-04 13:43:12 +0800

    优化了程序，提高读卡成功率

--- 配套程序/F103_霸道开发板/User/main.c
+++ 配套程序/F103_霸道开发板/User/main.c
@@ -23,82 +23,53 @@
 #include "rc522_config.h"
 #include "rc522_function.h"
 #include <stdbool.h>
-
-void IC_CMT ( uint8_t * UID, uint8_t * KEY, uint8_t RW, uint8_t * Dat );
 /**
   * @brief  IC测试函数
   * @param  无
   * @retval 无
   */
-uint8_t buffer[100];
-uint8_t buff1[] = {0x50 ,0xBA, 0xA9, 0x89};
-uint8_t buff2[]={0xFF ,0xFF, 0xFF, 0xFF, 0xFF, 0xFF};   // 卡A密钥
-uint8_t buff3[]={0x55 ,0xBA, 0xA9, 0x89, 0x50 ,0xBA, 0xA9, 0x89, 0x50 ,0xBA, 0xA9, 0x89, 0x50 ,0xBA, 0xA9, 0x89};    // 写入的数据
+uint8_t KeyValue[]={0xFF ,0xFF, 0xFF, 0xFF, 0xFF, 0xFF};   // 卡A密钥
 void IC_test ( void )
 {
+	uint32_t writeValue = 100;
+	uint32_t readValue;
 	char cStr [ 30 ];
   uint8_t ucArray_ID [ 4 ];    /*先后存放IC卡的类型和UID(IC卡序列号)*/                                                                                         
-	uint8_t ucStatusReturn;      /*返回状态*/                                                                                          
-  static uint8_t ucLineCount = 0; 
-  int i;
-  
+	uint8_t ucStatusReturn;      /*返回状态*/                                                                                           
   while ( 1 )
-  { 
-    /*******************卡片扇区读写测试****************************/
-    buff3[0]++;
-    
-    IC_CMT(buff1, buff2, 0, buff3);    // 写卡
-
-    IC_CMT(buff1, buff2, 1, buffer);   // 读卡
-    
-    printf ("读到的数据为：\n");
-    
-    for( i=0; i<16; i++)
-    {
-      printf ("0x%X\n", buffer[i]);    // 打印读到的数据
-    }
-    /*******************卡片扇区读写测试 END****************************/  
-    
+  {    
     /*寻卡*/
-		if ( ( ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID ) ) != MI_OK )  
+		if ( ( ucStatusReturn = PcdRequest ( PICC_REQIDL, ucArray_ID ) ) != MI_OK )  
        /*若失败再次寻卡*/
-			ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID );		                                                
+			ucStatusReturn = PcdRequest ( PICC_REQIDL, ucArray_ID );		                                                
 
 		if ( ucStatusReturn == MI_OK  )
 		{
       /*防冲撞（当有多张卡进入读写器操作范围时，防冲突机制会从其中选择一张进行操作）*/
 			if ( PcdAnticoll ( ucArray_ID ) == MI_OK )                                                                   
 			{
-				sprintf ( cStr, "The Card ID is: %02X%02X%02X%02X",
-                  ucArray_ID [ 0 ],
-                  ucArray_ID [ 1 ],
-                  ucArray_ID [ 2 ],
-                  ucArray_ID [ 3 ] );
-        
-        	printf ( "%s\r\n",cStr ); 
-
-				if ( ucLineCount == 0 )
-          LCD_SetTextColor(RED); 	
-				
-				ILI9341_DispStringLine_EN(LINE(ucLineCount) , (char* )cStr );
-       
-				IC_CMT(buff1, buff2, 1, buffer);   // 需要在读一次下次再能写进去（不知道为什么这样，应该是这样能改变某种模式吧）
-        
-				ucLineCount ++;
-				
-				if ( ucLineCount == 17 ) ucLineCount = 0;	    
-			}		
+				PcdSelect(ucArray_ID);			
+		
+				PcdAuthState( PICC_AUTHENT1A, 0x11, KeyValue, ucArray_ID );//校验密码 
+        WriteAmount(0x11,writeValue); //写入金额
+        if(ReadAmount(0x11,&readValue) == MI_OK)	//读取金额
+				{		
+					writeValue +=100;
+				  sprintf ( cStr, "The Card ID is: %02X%02X%02X%02X",ucArray_ID [0], ucArray_ID [1], ucArray_ID [2],ucArray_ID [3] );
+					printf ( "%s\r\n",cStr );  //打印卡片ID
+          ILI9341_DispStringLine_EN(LINE(0) , (char* )cStr ); //在屏幕上面显示ID
+					
+					printf ("余额为：%d\r\n",readValue);
+					sprintf ( cStr, "TThe residual amount: %d", readValue);				 										 	         
+					ILI9341_DispStringLine_EN(LINE(1) , (char* )cStr ); //在屏幕上面显示金额
+          PcdHalt();
+				}				
+			}				
 		}		
+		    
   }	
 }
-char PcdComMF522 ( uint8_t ucCommand, 
-                    uint8_t * pInData,
-                    uint8_t ucInLenByte, 
-                    uint8_t * pOutData, 
-                    uint32_t * pOutLenBit )	;
-uint8_t buff[] = {0x06, 0x61, 0x1, 0x2, 0x3, 0x4 ,0x6, 0x1, 0x2, 0x3, 0x4};
 
-uint32_t bufferlen;
 /**
   * @brief  主函数
   * @param  无
